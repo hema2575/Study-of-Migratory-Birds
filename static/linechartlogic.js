645a8e7c5f79f3e4d74d722f5a6109949e3d39c4
chilli <70721729+hema2575@users.noreply.github.com>
2021-02-01 22:14:28 -0600

    new files for trying out line charts

--- static/linechartlogic.js
+++ static/linechartlogic.js
@@ -1,83 +1,45 @@
 
+// function parseData(createGraph) {
+//     Papa.parse("./data/birdcount.csv", {
+//         download: true,
+//         complete: function (results) {
+//             //console.log(results.data); 
+//             createGraph(results.bdata);
+//         }
+//     })
+// }
 
-function findtotals(inputSpecies) {
 
-    var filterdata1 = birdData.filter(x => x.SPECIESNAME === inputSpecies)
+// // Parse local CSV file 
 
-    const regions = [];
-    const map = new Map();
-    for (const item of filterdata) {
-        if (!map.has(item.REGION)) {
-            map.set(item.REGION, true);    // set any value to Map
-            regions.push(
-                item.REGION
-            );
+// Papa.parse("./data/birdcount.csv", {
+//     complete: function (results) {
+//         console.log("Finished:", results.bdata);
+//         createGraph(results.bdata)
+//     }
+// });
+
+// //parseData(createGraph);
+
+
+var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+        columns: [
+            ['LWC', 30, 200, 100, 400, 150, 250],
+            ['EC', 50, 20, 10, 40, 15, 25],
+            ['UWC', 160, 45, 110, 410, 35, 210],
+            ['OIH', 10, 80, 130, 480, 350, 50]
+        ],
+        axis: {
+            y: {
+                label: {
+                    text: 'Y Label',
+                    position: 'outer-middle'
+                }
+            },
         }
     }
-    var years = [];
-    var totalbirdcountperyear = []
-    var birdcount1 = 0
-    // loop through regions array
-    for (var i = 0; i < regions.length; i++) {
-        // loop through the bird data
-        for (var j = 0; j < filterdata1.length; j++) {
-            // loop through birdcount data to sum totals for regions
-            if (filterdata1[j].REGION === regions[i]) {
-                // console.log('bird loop inner')
-                var birdcount1 = birdcount1 + parseInt(filterdata1[j].COUNT);
-            }
-        }
-        // append key item to dictionary
-        totalbirdcountperyear.push(birdcount1)
-        console.log(totalbirdcountperyear)
-        // reset birdcount to 0
-        var birdcount1 = 0
-    }
 
-    // create the function for the change event
-    function optionChanged(inputSpecies) {
-    getPlots(inputSpecies);
-    createGraph(inputSpecies);
-
-}
-
-    // for (var i = 1; i < filterData.length; i++) {
-    //     /* console.log(data[i][1]);  */
-    //     YEARS.push(filterData[i][0]);
-    //     tbcpy.push(filterData[i][2]);
-    // }
-
-}
-// create the function for the initial data rendering
-function init() {
-    //     select dropdown menu 
-    var dropdown = d3.select("#selDataset");
-
-    //     read the data 
-    const species = [];
-    const map = new Map();
-    for (const item of birdData) {
-        if (!map.has(item.SPECIESNAME)) {
-            map.set(item.SPECIESNAME, true);    // set any value to Map
-            species.push(
-                item.SPECIESNAME
-            );
-        }
-    }
-    //    get the id data to the dropdwown menu
-    species.forEach(function (name) {
-        dropdown.append("option").text(name).property("value");
-    });
-     //  call the function to display the data and the plots to the page
-    //  getPlots(species[0]);
-};
-
-
-init();
-
-    function close_window() {
-        if (confirm("Close Window?")) {
-        window.close();
-        }
-    }
+});
 
